f22e9fb66851562ba9c0714b956f75ca03088e3e
Jack Meier <jackson.meier423@gmail.com>
2020-06-19 22:09:42 -0700

    local storage error fix, color coding added

--- questions.md
+++
@@ -1,6 +0,0 @@
-How to capture user input after clicking on 'save' button
-How to compare times? Do we use inequalities?
-
-If one save button is clicked, should all inputs be saved? Or just the correspoding input?
-
-How to keep stuff from disappearing when refreshed.
--- script.js
+++ script.js
@@ -1,53 +1,47 @@
+storeValues();
+// Shows current date on webpage
 var now = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(now);
 
-var descriptionVal = [];
+// Save button 'click' actions
+$("#hour-9>.saveBtn").on("click", function () {
+  var nineVal = $("#hour-9>.description").val();
+  localStorage.setItem("nineEvent", nineVal);
+});
+$("#hour-10>.saveBtn").on("click", function () {
+  var tenVal = $("#hour-10>.description").val();
+  localStorage.setItem("tenEvent", tenVal);
+});
+$("#hour-11>.saveBtn").on("click", function () {
+  var elevenVal = $("#hour-11>.description").val();
+  localStorage.setItem("elevenEvent", elevenVal);
+});
+$("#hour-12>.saveBtn").on("click", function () {
+  var twelveVal = $("#hour-12>.description").val();
+  localStorage.setItem("twelveEvent", twelveVal);
+});
+$("#hour-13>.saveBtn").on("click", function () {
+  var oneVal = $("#hour-13>.description").val();
+  localStorage.setItem("oneEvent", oneVal);
+});
+$("#hour-14>.saveBtn").on("click", function () {
+  var twoVal = $("#hour-14>.description").val();
+  localStorage.setItem("twoEvent", twoVal);
+});
+$("#hour-15>.saveBtn").on("click", function () {
+  var threeVal = $("#hour-15>.description").val();
+  localStorage.setItem("threeEvent", threeVal);
+});
+$("#hour-16>.saveBtn").on("click", function () {
+  var fourVal = $("#hour-16>.description").val();
+  localStorage.setItem("fourEvent", fourVal);
+});
+$("#hour-17>.saveBtn").on("click", function () {
+  var fiveVal = $("#hour-17>.description").val();
+  localStorage.setItem("fiveEvent", fiveVal);
+});
 
-// console.log($(".description").length);
-var descriptions = [];
-
-for (i = 0; i < $(".description").length; i++) {
-  descriptions.push($(".description")[i]);
-}
-// var description = $(".description");
-// var description9 = $(".description")[0];
-// var description10 = $(".description")[1];
-// var description11 = $(".description")[2];
-// var description12 = $(".description")[3];
-// var description1 = $(".description")[4];
-// var description2 = $(".description")[5];
-// var description3 = $(".description")[6];
-// var description4 = $(".description")[7];
-// var description5 = $(".description")[8];
-
-// var input = description.html("hi");
-// description.css("background-color", "red");
-// description.css("border-style", "solid");
-
-$(".saveBtn").on("click", function () {
-  for (i = 0; i < $(".description").length; i++) {
-    descriptionVal.push(descriptions[i].value);
-    console.log(descriptions[i].value);
-  }
-
-  //   var nineEvent = descriptions[0].value;
-  //   var tenEvent = description10.value;
-  //   var elevenEvent = description11.value;
-  //   var twelveEvent = description12.value;
-  //   var oneEvent = description1.value;
-  //   var twoEvent = description2.value;
-  //   var threeEvent = description3.value;
-  //   var fourEvent = description4.value;
-  //   var fiveEvent = description5.value;
-  localStorage.setItem("nineEvent", descriptionVal[0]);
-  localStorage.setItem("tenEvent", descriptionVal[1]);
-  localStorage.setItem("elevenEvent", descriptionVal[2]);
-  localStorage.setItem("twelveEvent", descriptionVal[3]);
-  localStorage.setItem("oneEvent", descriptionVal[4]);
-  localStorage.setItem("twoEvent", descriptionVal[5]);
-  localStorage.setItem("threeEvent", descriptionVal[6]);
-  localStorage.setItem("fourEvent", descriptionVal[7]);
-  localStorage.setItem("fiveEvent", descriptionVal[8]);
+function storeValues() {
   var nineEvent = localStorage.getItem("nineEvent");
   var tenEvent = localStorage.getItem("tenEvent");
   var elevenEvent = localStorage.getItem("elevenEvent");
@@ -57,6 +51,131 @@ $(".saveBtn").on("click", function () {
   var threeEvent = localStorage.getItem("threeEvent");
   var fourEvent = localStorage.getItem("fourEvent");
   var fiveEvent = localStorage.getItem("fiveEvent");
-  alert(descriptionVal[0]);
-  alert(nineEvent);
-});
+  $("#hour-9>.description").val(nineEvent);
+  $("#hour-10>.description").val(tenEvent);
+  $("#hour-11>.description").val(elevenEvent);
+  $("#hour-12>.description").val(twelveEvent);
+  $("#hour-13>.description").val(oneEvent);
+  $("#hour-14>.description").val(twoEvent);
+  $("#hour-15>.description").val(threeEvent);
+  $("#hour-16>.description").val(fourEvent);
+  $("#hour-17>.description").val(fiveEvent);
+}
+
+var currentHour = moment().format("HH");
+
+console.log(currentHour);
+$(".description").css("border-style", "dashed");
+
+function colorCode() {
+    if (currentHour > 9 ) {
+        $("#hour-9>.description").css("background-color", "LightGrey");
+
+    }
+    else if (currentHour == 9) {
+        $("#hour-9>.description").css("background-color", "red");
+
+    }
+    else {
+        $("#hour-9>.description").css("background-color", "Lime");
+
+    }
+    if (currentHour > 10 ) {
+        $("#hour-10>.description").css("background-color", "LightGrey");
+
+    }
+    else if (currentHour == 10) {
+        $("#hour-10>.description").css("background-color", "red");
+
+    }
+    else {
+        $("#hour-10>.description").css("background-color", "Lime");
+
+    }    
+    if (currentHour > 11 ) {
+        $("#hour-11>.description").css("background-color", "LightGrey");
+
+    }
+    else if (currentHour == 11) {
+        $("#hour-11>.description").css("background-color", "red");
+
+    }
+    else {
+        $("#hour-11>.description").css("background-color", "Lime");
+
+    }    
+    if (currentHour > 12 ) {
+        $("#hour-12>.description").css("background-color", "LightGrey");
+
+    }
+    else if (currentHour == 12) {
+        $("#hour-12>.description").css("background-color", "red");
+
+    }
+    else {
+        $("#hour-12>.description").css("background-color", "Lime");
+
+    }    
+    if (currentHour > 13 ) {
+        $("#hour-13>.description").css("background-color", "LightGrey");
+
+    }
+    else if (currentHour == 13) {
+        $("#hour-13>.description").css("background-color", "red");
+
+    }
+    else {
+        $("#hour-13>.description").css("background-color", "Lime");
+
+    }    
+    if (currentHour > 14 ) {
+        $("#hour-14>.description").css("background-color", "LightGrey");
+
+    }
+    else if (currentHour == 14) {
+        $("#hour-14>.description").css("background-color", "red");
+
+    }
+    else {
+        $("#hour-14>.description").css("background-color", "Lime");
+
+    }    
+    if (currentHour > 15) {
+        $("#hour-15>.description").css("background-color", "LightGrey");
+
+    }
+    else if (currentHour == 15) {
+        $("#hour-15>.description").css("background-color", "red");
+
+    }
+    else {
+        $("#hour-15>.description").css("background-color", "Lime");
+
+    }    
+    if (currentHour > 16 ) {
+        $("#hour-16>.description").css("background-color", "LightGrey");
+
+    }
+    else if (currentHour == 16) {
+        $("#hour-16>.description").css("background-color", "red");
+
+    }
+    else {
+        $("#hour-16>.description").css("background-color", "Lime");
+
+    }    
+    if (currentHour > 17 ) {
+        $("#hour-17>.description").css("background-color", "LightGrey");
+
+    }
+    else if (currentHour == 17) {
+        $("#hour-17>.description").css("background-color", "red");
+
+    }
+    else {
+        $("#hour-17>.description").css("background-color", "Lime");
+
+    }    
+
+}
+colorCode();
